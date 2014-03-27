30326e7590161e54190a5a091b09159bb5321bf4
Ugochi <uofoha23@gmail.com>
2014-03-27 18:27:29 -0500

    Getting rid of string manipulation

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,18 +1,16 @@
 class Fizzbuzz
   def run(numbers)
-    numbers = numbers.split " "
     numbers.map! {|i| fizz_or_buzz i}
-    numbers.join " "
   end
 
   private 
 
   def fizz_or_buzz(number)
-    if number.to_i % 15 == 0
+    if number % 15 == 0
       return "FizzBuzz"
-    elsif number.to_i % 3 == 0 
+    elsif number % 3 == 0 
       return "Fizz"
-    elsif number.to_i % 5 == 0 
+    elsif number % 5 == 0 
       return "Buzz"
     end
     number
@@ -20,7 +18,7 @@ class Fizzbuzz
 end
 
 fb = Fizzbuzz.new
-numbers = (1..100).map { |i| i.to_s }
-print fb.run(numbers.join " ")
+numbers = (1..100).map { |i| i }
+print fb.run(numbers)
 
 
--- fizzbuzz_spec.rb
+++ fizzbuzz_spec.rb
@@ -3,19 +3,19 @@ require './fizzbuzz'
 describe Fizzbuzz, "run" do
   it "returns Fizz if number is divisible by 3" do
     fizzBuzz = Fizzbuzz.new
-    numbers = fizzBuzz.run("1 2 3")
-    numbers.should eq("1 2 Fizz")
+    numbers = fizzBuzz.run([1, 2, 3])
+    numbers.should eq([1, 2, "Fizz"])
   end
 
   it "returns Buzz if number is divisible by 5" do
     fizzBuzz = Fizzbuzz.new
-    numbers = fizzBuzz.run("5 10 2")
-    numbers.should eq("Buzz Buzz 2")
+    numbers = fizzBuzz.run([5, 10, 2])
+    numbers.should eq(["Buzz", "Buzz", 2])
   end
 
   it "returns FizzBuzz if number is divisible by 3 and 5" do
     fizzBuzz = Fizzbuzz.new
-    numbers = fizzBuzz.run("15 3 20")
-    numbers.should eq("FizzBuzz Fizz Buzz")
+    numbers = fizzBuzz.run([15, 3, 20])
+    numbers.should eq(["FizzBuzz", "Fizz", "Buzz"])
   end
 end
